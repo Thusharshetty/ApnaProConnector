bdc0bbd2769c453b64c5828d8de50f93a2820ff3
Thushar shetty <shettythushar31@gmail.com>
2026-07-07 06:45:35 +0530

    final

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -3,10 +3,15 @@ import "@/styles/globals.css";
 import { Provider } from "react-redux";
 import {store} from "@/config/redux/store.js"; 
 import { ToastContainer } from "react-toastify";
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return <>
   <Provider store={store}>
+    <Head>
+        <title>ProConnect</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */} 
+      </Head>
     <Component {...pageProps} />
     <ToastContainer />
   </Provider>
